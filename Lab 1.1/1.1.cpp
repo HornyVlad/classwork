0da10f9638225cd46698a38be58989f11b801794
HornyVlad <vlad.kochergin.work@mail.ru>
2021-09-13 13:26:12 +0300

    final

--- Lab 1.1/1.1.cpp
+++ Lab 1.1/1.1.cpp
@@ -2,11 +2,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
+#include <time.h>
 
 int main()
 {
 	setlocale(0, "");
-	int a[10] = {0,1,2,3,4,5,6,7,8,9}, b;
-	b = abs(a[0] - a[9]);
-	printf("Разница между максимальным и минимальным = %d", b);
+	int a[10], b, i, max = 0, min = 20;
+	srand(time(NULL));
+	printf("Массив: ");
+	for (i = 0; i < 10; i++)
+	{
+		a[i] = rand() % 21 - 10;
+		printf("%d ", a[i]);
+	}
+	for (i = 0; i < 10; i++)
+	{
+		if (a[i] > max)
+		{
+			max = a[i];
+		}
+	}
+	for (i = 0; i < 10; i++)
+	{
+		if (a[i] < min)
+		{
+			min = a[i];
+		}
+	}
+	b = max - min;
+	printf("\nРазница между максимальным и минимальным = %d", b);
 }
--- Lab 1.3/1.3.cpp
+++ Lab 1.3/1.3.cpp
@@ -2,23 +2,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
-#define i 20
 
 int main()
 {
 	setlocale(0, "");
 	srand(time(NULL));
-	int a[i];
-	int max = rand() % 6;
-	printf("Размер массива: %d\n", max);
-	printf("Заполните массив: ");
-	for (int n = 0; n < max; n++)
+	int** z;
+	int max1 = rand() % 7 + 1;
+	printf("Количество строк: %d\n", max1);
+	z = (int**)malloc(max1 * sizeof(int));
+	int max2 = rand() % 7 + 1;
+	printf("Количество столбцов: %d\n", max2);
+
+	for (int i = 0; i < max2; i++)
 	{
-		scanf_s("%d", &a[n]);
+		z[i] = (int*)malloc(max2 * sizeof(int));
 	}
-	printf("Массив: ");
-	for (int k = 0; k < max; k++)
+	printf("Заполните массив: ");
+	for (int n = 0; n < max1; n++)
 	{
-		printf("%d ", a[k]);
+		for (int m = 0; m < max2; m++)
+		{
+			z[m][n] = rand() % 20;
+		}
+	}
+	printf("Массив: \n");
+	for (int n = 0; n < max1; n++)
+	{
+		for (int m = 0; m < max2; m++)
+		{
+			printf("%d ", z[m][n]);
+		}
+		printf("\n");
 	}
 }
